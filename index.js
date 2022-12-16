384bb1b4aaadaa809a73b1dec80cb035823283ed
Your Name <you@example.com>
2022-12-16 18:41:55 +0300

    добавлено немного комментов

--- index.js
+++ index.js
@@ -3,26 +3,26 @@ const express = require('express')
 const router = require('./routes/index')
 const cors = require('cors')
 const fileUpload = require('express-fileupload')
-// const models = require('./models/models')
-const errorHandler = require('./middleware/errorHandlingMiddleware')
-const PORT = process.env.PORT || 4000
+const errorHandler = require('./middleware/errorHandlingMiddleware') // обработчик ошибок
+const PORT = process.env.PORT || 4000 // определим порт приложения
 const path = require('path')
 const sequelize = require('./db')
 
-const app = express()
+const app = express() // создаем приложение
 
-app.use(cors())
-app.use(express.json())
+app.use(cors()) // чтобы не было проблем с cors
+app.use(express.json()) // для парсинга json
 app.use(express.static(path.resolve(__dirname, 'static')))
-app.use(fileUpload({}))
-app.use('/api', router)
+app.use(fileUpload({})) // загрузчик файлов
+app.use('/api', router) // по роуту /api определяем корневой роутер приложения
 
 // обработка ошибок, последний middleware
 app.use(errorHandler)
 
 const start = async () => {
+  // функция запускающая приложение
   try {
-    await sequelize.authenticate()
+    await sequelize.authenticate() // подключаемся к БД
     await sequelize.sync()
     app.listen(PORT, () => {
       console.log(`Server started on port: ${PORT}`)
